1360794a8b94a7f3a092c1a17ad9f37f96183b74
Brn <brn.olives@gmail.com>
2025-07-30 12:07:11 -0300

    style: fix linting issues

--- src/model_training.py
+++ src/model_training.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 
+
 def train_anomaly_detector(data: pd.DataFrame) -> IsolationForest:
     """
     Treina um modelo IsolationForest nos dados fornecidos.
     """
-    model = IsolationForest(contamination='auto', random_state=42)
-    model.fit(data[['value']])
+    model = IsolationForest(contamination="auto", random_state=42)
+    model.fit(data[["value"]])
     return model
+
 
 def predict_anomalies(model: IsolationForest, data: pd.DataFrame) -> pd.DataFrame:
     """
     Usa um modelo treinado para prever anomalias.
     """
-    data['anomaly'] = model.predict(data[['value']])
+    data["anomaly"] = model.predict(data[["value"]])
     return data
