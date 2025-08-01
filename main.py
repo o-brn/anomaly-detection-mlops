1673fb1a7037a775b093aade06dfad93fa5f7099
Brn <brn.olives@gmail.com>
2025-08-01 12:24:26 -0300

    style: format code with ruff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def main() -> None:
     print("Hello from anomaly-detection-mlops!")
 
 
--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -5,7 +5,7 @@ from src.data_processing import load_and_preprocess_data
 
 
 @pytest.fixture
-def sample_csv_file(tmp_path):
+def sample_csv_file(tmp_path) -> None:
     """
     Cria um arquivo CSV temporário para os testes.
     Usa a fixture 'tmp_path' do pytest para criar um diretório temporário.
@@ -16,7 +16,7 @@ def sample_csv_file(tmp_path):
     return str(file_path)
 
 
-def test_load_and_preprocess_data(sample_csv_file):
+def test_load_and_preprocess_data(sample_csv_file) -> pd.DataFrame:
     # Arrange (Organizar): A fixture 'sample_csv_file' já preparou o ambiente.
 
     # Act (Agir): Chamar a função que está sendo testada.
